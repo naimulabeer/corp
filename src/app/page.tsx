6e8b8f807515608bf9db75352eda5f1683ba8029
naimulabeer <naimul.h.abeer@gmail.com>
2023-12-31 05:13:51 +0600

    adding reusable presentation component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 import homeImg from "/public/home.jpg";
 
 export default function Home() {
   return (
-    <div className="absolute -z-10 inset-0">
+    <div>
       Home Page
-      <Image
-        src={homeImg}
-        alt="car factory"
-        fill
-        style={{ objectFit: "cover" }}
+      <Hero
+        imgData={homeImg}
+        imgAlt="car-factory"
+        title="Professional Cloud Hosting"
       />
     </div>
   );
